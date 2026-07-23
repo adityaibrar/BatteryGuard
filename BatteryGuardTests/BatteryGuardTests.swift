7d033a900e090354e0475830a9b78a66385d53ef
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2026-07-23 14:10:15 +0700

    Initial Commit

---
+++ BatteryGuardTests/BatteryGuardTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryGuardTests.swift
+//  BatteryGuardTests
+//
+//  Created by Ibrar on 23/07/26.
+//
+
+import Testing
+@testable import BatteryGuard
+
+struct BatteryGuardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
